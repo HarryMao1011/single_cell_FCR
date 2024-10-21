d8bf63942996dceebaba2681683db88ac7136f94
HarryMao1011 <harrymao1011@gmail.com>
2024-10-21 13:08:51 -0400

    update all

--- main.sh
+++ main.sh
@@ -8,8 +8,8 @@ DATA=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 PYARGS=""
 PYARGS="$PYARGS --name train_epoch-1000"
 PYARGS="$PYARGS --artifact_path $DATA/artifact"
-PYARGS="$PYARGS --data_path $DATA/path/to/datasets" #sciplex_prepped.h5ad, L008_prepped.h5ad
-PYARGS="$PYARGS --gpu 0" #PYARGS="$PYARGS --cpu"
+PYARGS="$PYARGS --data_path $DATA/path/to/datasets" 
+PYARGS="$PYARGS --gpu 0" 
 
 PYARGS="$PYARGS --omega1 1.0"
 PYARGS="$PYARGS --omega2 1.0"
@@ -21,7 +21,7 @@ PYARGS="$PYARGS --dist_mode match"
 PYARGS="$PYARGS --decode_aggr dot"
 
 PYARGS="$PYARGS --max_epochs 1000"
-PYARGS="$PYARGS --batch_size 64"
+PYARGS="$PYARGS --batch_size 1024"
 PYARGS="$PYARGS --eval_mode classic"
 
 python main.py $PYARGS
